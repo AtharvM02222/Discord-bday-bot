5006c5d372f88045c8f78a8f07d536ae6c966147
Atharv Mandlavdiya <atharvam682@gmail.com>
2026-06-29 08:58:47 +0530

    Update birthday.js

---
+++ Bot/birthday.js
@@ -0,0 +1,9 @@
+const PREFIX = "!bday ";
+
+module.exports = async function (message) {
+  if (!message.content.startsWith(PREFIX)) return;
+  if (message.author.bot) return;
+
+  const bdate = message.content.slice(6).trim();
+  const userid = message.author.id;
+};
