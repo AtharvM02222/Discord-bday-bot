a461164e0dcc08789cfe5d4339aa5c155b176ae7
Atharv Mandlavdiya <atharvam682@gmail.com>
2026-06-29 09:17:51 +0530

    yup

--- Bot/notion.js
+++ Bot/notion.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const { Client } = require("@notionhq/client");
 //https://ripe-jackfruit-476.notion.site/38efc2bedce480b98d4efb603e92b8ef?v=38efc2bedce48083955d000cd261178f&source=copy_link
 const notion = new Client({
