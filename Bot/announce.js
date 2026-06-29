b4ddbede16e583bfc64b8ef46ca94d3beea6c445
Atharv Mandlavdiya <atharvam682@gmail.com>
2026-06-29 10:15:13 +0530

    small work

---
+++ Bot/announce.js
@@ -0,0 +1,8 @@
+const notion = require("./notion");
+
+/*
+if (day === todayDay && month === todayMonth) {
+  const channel = await client.channels.fetch(process.env.ANNOUNCE_CHANNEL_ID);
+  await channel.send(`🎉 @everyone, today is <@${discordId}>'s birthday!`);
+}
+*/
--- Bot/birthday.js
+++ Bot/birthday.js
@@ -11,4 +11,5 @@ module.exports = async function (message) {
 
   await notion.createEntry(userid, bdate);
   message.react("👍");
+  //for ts we will use this emoji :ts:
 };
