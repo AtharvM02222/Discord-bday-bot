b19c894cac4ca18dce0452c5e5890f9d43104998
Atharv Mandlavdiya <atharvam682@gmail.com>
2026-06-29 08:58:44 +0530

    Update notion.js

---
+++ Bot/notion.js
@@ -0,0 +1,5 @@
+const { Client } = require("@notionhq/client");
+//https://ripe-jackfruit-476.notion.site/38efc2bedce480b98d4efb603e92b8ef?v=38efc2bedce48083955d000cd261178f&source=copy_link
+const notion = new Client({
+  auth: process.env.NOTION_API,
+});
